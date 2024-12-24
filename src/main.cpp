30dd3fabe744ee48813a93ade95b427d54cf5dda
Malik Endsley <malik.s.endsley@gmail.com>
2024-12-24 02:23:33 -0500

    infra for multiple tile types

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@
 #include <vector>
 #include <numeric>
 #include <SDL_mixer.h>
+#include <vector>
+#include <functional>
 
-constexpr size_t simScale = 5; // "pixels" per sand grain. This is handled as simulation then integer scaling.
-const size_t windowHeight = 400;
-const size_t windowWidth = 400;
+constexpr size_t simScale = 10; // "pixels" per sim grain. This is handled as simulation then integer scaling.
+const size_t windowHeight = 800;
+const size_t windowWidth = 800;
+
+// TODO: Eventually, we will parallelize on chunks, so make it parametric on height and width
 const int simHeight = windowHeight / simScale;
 const int simWidth = windowWidth / simScale;
 const int approxTargetFPS = 142;
@@ -15,9 +19,9 @@ const int approxTargetFPS = 142;
 Mix_Chunk *sandSound = nullptr;
 
 // Function declarations (TODO: Fix this later)
-static void updateSandTexture(SDL_Texture &texture, std::unique_ptr<int[]> &sand, int scaling, int simWidth, int simHeight);
+static void updateSandTexture(SDL_Texture &texture, std::unique_ptr<int[]> &sim, int scaling, int simWidth, int simHeight);
 static void updateUI(SDL_Texture &texture, int scaling, long long frame);
-static int simulateSand(std::unique_ptr<int[]> &sand);
+static void simulateFalling(int x, int y, std::unique_ptr<int[]> &sim, int width = simWidth, int height = simHeight);
 static inline int idx(int x, int y, int width);
 
 int main(int argc, char *argv[])
@@ -39,7 +43,12 @@ int main(int argc, char *argv[])
 
 	SDL_Texture *screenTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, windowWidth, windowHeight);
 
-	auto sand = std::make_unique<int[]>(simHeight * simWidth);
+	std::vector<std::function<void(int, int, std::unique_ptr<int[]> &, int, int)>> passes = {
+		[](int, int, std::unique_ptr<int[]> &, int, int) {}, // no-op pass
+		simulateFalling,
+	};
+
+	auto sim = std::make_unique<int[]>(simHeight * simWidth);
 
 	bool running = true;
 	SDL_Event event;
@@ -49,10 +58,32 @@ int main(int argc, char *argv[])
 	// TODO: Struct this hoe
 	auto mouseX = 0;
 	auto mouseY = 0;
-	auto lmbDown = false;
+
+	struct keyDownState
+	{
+		bool lmb = false;
+		bool oneKey = false;
+		bool twoKey = false;
+		bool threeKey = false;
+		bool fourKey = false;
+		bool cKey = false;
+	};
+
+	enum simTile
+	{
+		sand,
+		stone,
+		water,
+		gas,
+	};
+	simTile currentTile = sand;
 
 	while (running)
 	{
+
+		// Alternative is to declare this outside
+		keyDownState input;
+		// Process events
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_QUIT)
@@ -60,44 +91,87 @@ int main(int argc, char *argv[])
 				std::cout << "Quitting" << std::endl;
 				running = false;
 			}
+			// populate the struct with the current state of the keys
+			if (event.type == SDL_KEYDOWN)
+			{
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_1:
+					input.oneKey = true;
+					break;
+				case SDLK_2:
+					input.twoKey = true;
+					break;
+				case SDLK_3:
+					input.threeKey = true;
+					break;
+				case SDLK_4:
+					input.fourKey = true;
+					break;
+				case SDLK_c:
+					input.cKey = true;
+					break;
+				}
+			}
 		}
 
 		// Handle mouse input
 		auto click = SDL_GetMouseState(&mouseX, &mouseY);
-		lmbDown = click & SDL_BUTTON(SDL_BUTTON_LEFT);
-		if (lmbDown)
+		input.lmb = click & SDL_BUTTON(SDL_BUTTON_LEFT);
+		if (input.lmb)
 		{
-			std::cout << "Mouse down at (" << mouseX << ", " << mouseY << ")" << std::endl;
-			// spawn some sand at the mouse
+			// spawn the held tile at the mouse position (currently just sand)
 			if (mouseX >= 0 && mouseX < windowWidth && mouseY >= 0 && mouseY < windowHeight)
 			{
-				sand[(mouseY / simScale) * simWidth + (mouseX / simScale)] = 1;
+				sim[(mouseY / simScale) * simWidth + (mouseX / simScale)] = 1;
 			}
+		}
+
+		// Handle keyboard input
+		if (input.oneKey)
+		{
+			currentTile = sand;
+			std::cout << "Current tile: sand" << std::endl;
+		}
+		if (input.twoKey)
+		{
+			currentTile = stone;
+			std::cout << "Current tile: stone" << std::endl;
+		}
+		if (input.threeKey)
+		{
+			currentTile = water;
+			std::cout << "Current tile: water" << std::endl;
+		}
+		if (input.fourKey)
+		{
+			currentTile = gas;
+			std::cout << "Current tile: gas" << std::endl;
+		}
+		if (input.cKey)
+		{
+			std::fill(sim.get(), sim.get() + simHeight * simWidth, 0);
+			std::cout << "Cleared the simulation" << std::endl;
 		}
 
 		// The beginnings of a rendering pipeline
 
 		// Update Textures
-		updateSandTexture(*screenTexture, sand, simScale, simWidth, simHeight);
+		updateSandTexture(*screenTexture, sim, simScale, simWidth, simHeight);
 		updateUI(*screenTexture, simScale, simTick);
 
 		// Draw the texture
 		SDL_RenderCopy(renderer, screenTexture, nullptr, nullptr);
 		SDL_RenderPresent(renderer);
 
-		// Step the sim
-		if (simulateSand(sand))
+		// Iterate bottom up on the simulation grid
+		for (int y = simHeight - 1; y >= 0; y--)
 		{
-			if (Mix_Playing(0) == 0)
+			for (int x = 0; x < simWidth; x++)
 			{
-				Mix_PlayChannel(0, sandSound, -1);
+				passes[sim[y * simWidth + x]](x, y, sim, simWidth, simHeight);
 			}
 		}
-		else
-		{
-			Mix_HaltChannel(0);
-		}
-
 		simTick++;
 
 		// TODO: I will need to properly accumulate time up to this point for true framerate,
@@ -113,58 +187,59 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-static int simulateSand(std::unique_ptr<int[]> &sand)
+// Right now sand's only behavior is to fall, but later on, we will have different types of particles
+static void simulateFalling(int x, int y, std::unique_ptr<int[]> &grid, int width, int height)
 {
-	// TODO: This is just for fun, remove it.
-	auto moved = 0;
+	// TODO: Just for fun, remove later
+	auto moved = false;
 
-	for (int y = simHeight - 1; y >= 0; y--)
+	// Sand collides with the bottom of the screen
+	if (y == height - 1)
+		return;
+	// Sand tries to fall down
+	if (grid[(y + 1) * width + x] == 0)
 	{
-		for (int x = 0; x < simWidth; x++)
+		grid[y * width + x] = 0;
+		grid[(y + 1) * width + x] = 1;
+		moved = true;
+	}
+	else
+	{
+		// If the pixel below is not empty, pick a random direction and move the grain there, if possible
+		int direction = rand() % 2;
+		if (direction == 0)
 		{
-			if (sand[y * simWidth + x] == 1)
+			// Move the grain to the left
+			if (x > 0 && grid[(y + 1) * width + (x - 1)] == 0)
 			{
-				// Sand collides with the bottom of the screen
-				if (y == simHeight - 1)
-					continue;
-
-				// Sand tries to fall down
-				if (sand[(y + 1) * simWidth + x] == 0)
-				{
-					sand[y * simWidth + x] = 0;
-					sand[(y + 1) * simWidth + x] = 1;
-					moved++;
-				}
-				else
-				{
-					// If the pixel below is not empty, pick a random direction and move the grain there, if possible
-					int direction = rand() % 2;
-					if (direction == 0)
-					{
-						// Move the grain to the left
-						if (x > 0 && sand[(y + 1) * simWidth + (x - 1)] == 0)
-						{
-							sand[y * simWidth + x] = 0;
-							sand[(y + 1) * simWidth + (x - 1)] = 1;
-							moved++;
-						}
-					}
-					else
-					{
-						// Move the grain to the right
-						if (x < simWidth - 1 && sand[(y + 1) * simWidth + (x + 1)] == 0)
-						{
-							sand[y * simWidth + x] = 0;
-							sand[(y + 1) * simWidth + (x + 1)] = 1;
-							moved++;
-						}
-					}
-				}
+				grid[y * width + x] = 0;
+				grid[(y + 1) * width + (x - 1)] = 1;
+				moved = true;
+			}
+		}
+		else
+		{
+			// Move the grain to the right
+			if (x < width - 1 && grid[(y + 1) * width + (x + 1)] == 0)
+			{
+				grid[y * width + x] = 0;
+				grid[(y + 1) * width + (x + 1)] = 1;
+				moved = true;
 			}
 		}
 	}
 
-	return moved;
+	if (moved)
+	{
+		if (Mix_Playing(0) == 0)
+		{
+			Mix_PlayChannel(0, sandSound, -1);
+		}
+	}
+	else
+	{
+		Mix_HaltChannel(0);
+	}
 }
 
 static void updateSandTexture(SDL_Texture &texture, std::unique_ptr<int[]> &sand, int scaling, int simWidth, int simHeight)
